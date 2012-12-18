a5a4d583a396027b0800c767dc445c75afc7af07
Jubjub <ethedona@gmail.com>
2012-12-18 22:50:47 +0100

    added basic block class

--- js/jenga.js
+++ js/jenga.js
@@ -1,8 +1,27 @@
+/* basic construction piece */
+Block = (function () {
+  function constructor(x, y, width, height) {
+    this.sprite = new Sprite("assets/img/debug.png", x, y);
+  }
+
+  constructor.prototype = {
+  }
+
+  return constructor;
+})();
+
 /* main game state */
 function PlayState() {
   this.setup = function() {
+    this.blocks = new SpriteList();
+    block = new Block(20, 20, 40, 20);
+    this.addBlock(block);
     this.test = new Sprite("assets/img/debug.png", 100, context.height / 2 - 100);
     preventKeys("down", "right", "left", "right", "space");
+  }
+
+  this.addBlock = function(block) {
+    this.blocks.push(block.sprite);
   }
 
   this.update = function() {
@@ -16,7 +35,9 @@ function PlayState() {
   this.draw = function() {
     clearCanvas();
     this.test.draw();
+    this.blocks.draw();
   }
+
 }
 
 var playState = new PlayState();
