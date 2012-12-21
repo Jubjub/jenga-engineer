871e94d54a4f399dac6e793f55678077ebd9a18f
David Gomes <davidrafagomes@gmail.com>
2012-12-21 18:22:37 +0000

    Small changes on server/.

--- server/jenga.js
+++
@@ -1,9 +0,0 @@
-var io = require("socket.io").listen(80, '0.0.0.0');
-
-io.sockets.on("connection", function(socket) {
-  var address = socket.handshake.address;
-  console.log("new connection from " + address.address + ":" + address.port);
-  socket.on("hello", function(msg) {
-    console.log("reported link from '" + msg.source + "' version");
-  });
-});
