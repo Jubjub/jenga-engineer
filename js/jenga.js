f2332c38bb33844edacaf59eee88476912c6be09
David Gomes <davidrafagomes@gmail.com>
2012-12-21 18:25:50 +0000

    Splitted and organized code a little bit.

--- js/jenga.js
+++ js/jenga.js
@@ -95,35 +95,6 @@ Block = (function () {
   return constructor;
 })();
 
-/* intro menu state, including setting up the network link */
-function MenuState() {
-  this.setup = function() {
-    this.global = new SpriteList();
-    this.label = new Sprite(null, 50, 0);
-    this.label.makeLabel("Jenga Engineer", 70, "Verdana", "black");
-    this.global.push(this.label);
-    preventKeys("down", "right", "left", "right", "space");
-    /* establish a link to the server */
-    socket = io.connect("http://localhost");
-    socket.emit("hello", { source: "development" });
-  }
-
-  this.update = function() {
-    if (isDown("space")) {
-      var state = new PlayState();
-      switchState(state);
-    }
-    if (isDown("space")) {
-
-    }
-  }
-
-  this.draw = function() {
-    clearCanvas();
-    this.global.draw();
-  }
-}
-
 /* main game state */
 function PlayState() {
   this.setup = function() {
