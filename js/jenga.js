69bd9d8ca38c1e56de1ada73472719ac3450fdc8
Jubjub <ethedona@gmail.com>
2012-12-24 18:04:07 +0100

    added base and removed unneeded sprites

--- js/jenga.js
+++ js/jenga.js
@@ -176,14 +176,18 @@ function PlayState() {
     /* Add some test blocks */
     //block = new Block(20, 20, 40, 20);
     //this.addBlock(block);
-    this.addBlock(new Block(40, 80, 80, 10));
-    this.addBlock(new Block(140, 80, 80, 10));
-    this.addBlock(new Block(240, 80, 80, 10));
+    //this.addBlock(new Block(40, 80, 80, 10));
+    //this.addBlock(new Block(140, 80, 80, 10));
+    this.addBlock(new Block(context.width / 2 - 300 / 2, 80, 300, 20));
+    /*
     this.test = new Sprite("assets/img/debug.png", 100, context.height / 2 - 100);
     this.test.stampRect(14, 0, 4, 16, "#0000ff");
+    */
+    /*
     this.proc = new Sprite(null, 300, 300);
     this.proc.makeGraphic(160, 20, "#ccff11");
     this.proc.stampText(0, 0, "hello!", 16, "Calibri", "#333333");
+    */
     this.hintBlock = new Sprite(null, 0, 0);
     this.hintBlock.alpha = 0.5;
 
@@ -267,9 +271,7 @@ function PlayState() {
       this.hintBlock.draw();
     }
 
-    this.test.draw();
     this.blockss.draw();
-    this.proc.draw();
   }
 }
 
