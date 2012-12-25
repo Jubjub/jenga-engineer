de78800b166e03075c28fa73c542237647990463
David Gomes <davidrafagomes@gmail.com>
2012-12-25 23:50:57 +0000

    Separated a lot of the code, more work towards testing.html.

---
+++ js/Constants.js
@@ -0,0 +1,5 @@
+/* Constants */
+var verlet_steps = 3;
+var damping = 0.9994;
+var radToDeg = 180 / Math.PI;
+var degToRad = Math.PI / 180;
---
+++ js/Utils.js
@@ -0,0 +1,30 @@
+/* Utils */
+function vec2Angle(v1, v2) {
+  return Math.atan2(v2.y - v1.y, v2.x - v1.x) * radToDeg;
+}
+
+function vec2Dot(v1, v2) {
+  return (v1.x * v2.x + v1.y * v2.y);
+}
+
+function vec2Length(v1) {
+  return Math.sqrt(v1.x * v1.x + v1.y * v1.y);
+}
+
+function vec2Normalize(v1) {
+  var l = vec2Length(v1);
+  v1.x /= l;
+  v1.y /= l;
+}
+
+function intervalDistance(proj1, proj2) {
+  if (proj1[0] < proj2[0]) {
+    return proj2[0] - proj1[1];
+  } else {
+    return proj1[0] - proj2[1];
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
