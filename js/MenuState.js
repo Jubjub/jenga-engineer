9ddcf0da950e94c1ed5050ef3b37c9d4ed21cb99
Jubjub <ethedona@gmail.com>
2012-12-29 16:44:13 +0100

    added menu bg

--- js/MenuState.js
+++ js/MenuState.js
@@ -3,7 +3,7 @@
 function MenuState() {
   this.setup = function() {
     preventKeys("down", "right", "left", "right", "space");
-
+    this.bg = new Sprite("assets/img/menu.png", -8, 0);
   }
 
   this.update = function() {
@@ -14,9 +14,12 @@ function MenuState() {
 
   this.draw = function() {
     clearCanvas();
+    this.bg.draw();
 
+    /*
     currentFont = "70px Verdana";
     drawString("Jenga Engineer", 55, 80, "black");
+    */
   }
 }
 
