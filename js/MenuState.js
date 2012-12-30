6974a0058219fe1f3fcd928d19c93a5a87841481
Jubjub <ethedona@gmail.com>
2012-12-30 17:58:18 +0100

    fixed starting bug

--- js/MenuState.js
+++ js/MenuState.js
@@ -3,14 +3,18 @@
 function MenuState() {
   this.setup = function() {
     preventKeys("down", "right", "left", "right", "space");
+    this.starting = false;
     this.bg = new Sprite("assets/img/menu.png", -8, 0);
   }
 
   this.update = function() {
     if (isDown("space") || isMouseDown("left")) {
-      setTimeout(function() {
-        switchState(new PlayState());
-      }, 500);
+      if (!this.starting) {
+        this.starting = true;
+        setTimeout(function() {
+          switchState(new PlayState());
+        }, 500);
+      }
     }
   }
 
